e138b65a7d0756499c14a0335fd1ad99138a2145
kevynbct <kevyn.buchet@gmail.com>
2019-02-20 14:51:41 +0100

    Ajout du loader dans les détails

--- app/src/main/java/adykb/android/mobiapps/PokeDetailActivity.kt
+++ app/src/main/java/adykb/android/mobiapps/PokeDetailActivity.kt
@@ -2,11 +2,17 @@ package adykb.android.mobiapps
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
+import me.sargunvohra.lib.pokekotlin.client.PokeApiClient
+import me.sargunvohra.lib.pokekotlin.model.Pokemon
 
-class PokeDetailActivity : AppCompatActivity() {
+class PokeDetailActivity : AppCompatActivity(){
+
     lateinit var textViewId : TextView
     lateinit var textViewName : TextView
+    lateinit var pokemon : Pokemon
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +21,23 @@ class PokeDetailActivity : AppCompatActivity() {
         textViewId = findViewById(R.id.detail_id)
         textViewName = findViewById(R.id.detail_name)
 
-        textViewId.text = intent.getIntExtra(ARG_ID, -1).toString()
-        textViewName.text = intent.getStringExtra(ARG_NAME)
+        textViewId.visibility = View.INVISIBLE
+        textViewName.visibility = View.INVISIBLE
 
+        Thread {
+            findViewById<ProgressBar>(R.id.progressBar).visibility = View.VISIBLE
+            pokemon = PokeApiClient().getPokemon(intent.getIntExtra(ARG_ID, -1))
+
+            runOnUiThread {
+                findViewById<ProgressBar>(R.id.progressBar).visibility = View.INVISIBLE
+
+                textViewId.text = pokemon.id.toString()
+                textViewName.text = pokemon.name
+
+                textViewId.visibility = View.VISIBLE
+                textViewName.visibility = View.VISIBLE
+            }
+        }.start()
     }
 
     companion object {
